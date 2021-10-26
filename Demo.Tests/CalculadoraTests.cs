d70505ad722149abc909a139a25b66d69f5b68f3
IgorTorres <itorres@provertec.com.br>
2021-10-25 21:03:03 -0300

    Desenvolvido a entidade base e a classe Teste para a entidade.

---
+++ Demo.Tests/CalculadoraTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Demo.Tests
+{
+    public class CalculadoraTests
+    {
+        [Fact] //Indicando que é um método de teste.
+        public void Calculadora_Soma_RetornarValorSoma()
+        {
+            //Nomenclatura padrão utilizada Objeto_Método_OQueEspera.
+            //Padrão AAA
+
+            //Arrange
+            var calculadora = new Calculadora();
+
+            //Act
+            var resultado = calculadora.Somar(2, 2);
+
+            //Assert
+            Assert.Equal(4, resultado);
+        }
+    }
+}

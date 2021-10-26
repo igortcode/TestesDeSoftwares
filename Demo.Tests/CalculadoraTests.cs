cfe165e14cecc166c5166ec6d0e2b58244403033
IgorTorres <itorres@provertec.com.br>
2021-10-25 21:21:57 -0300

    Adicionado o método que consegue testar vários cenários

--- Demo.Tests/CalculadoraTests.cs
+++ Demo.Tests/CalculadoraTests.cs
@@ -19,5 +19,26 @@ namespace Demo.Tests
             //Assert
             Assert.Equal(4, resultado);
         }
+
+        //Testando em vários cenários
+        [Theory]
+        [InlineData(1,1,2)] //Parametros a serem testados.
+        [InlineData(2, 5, 7)]
+        [InlineData(6, 6, 12)]
+        [InlineData(8, 9, 17)]
+        [InlineData(4, 3, 7)]
+        [InlineData(5, 8, 13)]
+        [InlineData(6, 2, 8)]
+        public void Calculadora_Somar_RetornarValoresSomaCorretos(double v1, double v2, double total)
+        {
+            //Arrange
+            var calculadora = new Calculadora();
+
+            //Act
+            var resultado = calculadora.Somar(v1, v2);
+
+            //Assert
+            Assert.Equal(total, resultado);
+        }
     }
 }
